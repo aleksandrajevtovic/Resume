be581fc6aa442f9546940e3d0c1c2d84f804852b
sanja048 <sanja.048@gmail.com>
2022-10-09 21:52:15 +0200

    scrolltrigger for about section, new project images and description

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import gsap from 'gsap';
 
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { DOCUMENT } from '@angular/common';
+
+gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
-  constructor() { }
+  tlH = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.about-page',
+      markers: { startColor: 'yellow', endColor: 'yellow' },
+      scrub: true,
+      start: '3%',
+      end: '15%',
+    },
+  });
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.scrollAnim();
+    this.scrollAnimRemove();
+  }
+  scrollAnim(): void {
+    this.tlH.fromTo('.hero h1', { opacity: 1 }, { opacity: 0 });
+    this.tlH.fromTo(
+      '.highlight',
+      { color: 'rgba(255,255,255,0.2' },
+      { color: 'rgba(255,255,255,1', stagger: 1 }
+    );
   }
 
+  tlHRemove = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.about-page',
+      markers: { startColor: 'pink', endColor: 'pink' },
+      scrub: true,
+      start: '7%',
+      end: '20%',
+    },
+  });
+  scrollAnimRemove(): void {
+    this.tlHRemove.to('.highlight', { color: 'rgba(255,255,255,0.4', stagger: 1 });
+  }
 }
