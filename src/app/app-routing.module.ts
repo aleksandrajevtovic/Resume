a3a2274a72feef35bdcac30c7974b7984670a3c2
sanja048 <sanja.048@gmail.com>
2024-12-12 20:31:23 +0100

    router; from 16 to 17; zone.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full', redirectTo: '' },
-  { path: '**', component: ErrorComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' }, // pathMatch is explicitly 'full'
+  { path: '**', component: ErrorComponent }, // No pathMatch needed for wildcard route
 ];
 
 @NgModule({
